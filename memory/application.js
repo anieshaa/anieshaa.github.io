7f808e3cd4600bf8573b1e1612e6393cdff03e8b
anieshaa <aallen1@kippnyccp.org>
2015-03-12 08:38:39 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
+  var card_array = ["blue", "blue", "red", "red", "yellow", "yellow", "green", "green", "orange", "orange", "purple", "purple", "white", "white", "pink", "pink", "brown", "brown", "magenta", "magenta"]
 
   for(i in card_array){
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
