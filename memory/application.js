38610c38e443dc9e72c6ef4f0420d3a6e2f97d86
anieshaa <aallen1@kippnyccp.org>
2015-03-13 09:02:55 -0400

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
   var card_array = ["blue", "blue", "red", "red", "yellow", "yellow", "green", "green", "orange", "orange", "purple", "purple", "white", "white", "pink", "pink", "brown", "brown", "magenta", "magenta"]
-
-  for(i in card_array){
+  
+var click_count = 0;
+  for(i in card_array){ //creates the cards
     $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
-  $('.card').click(function(){
-    $(this).find('p').css("opacity", 1);
+  $('.card').click(function(){ //when card is clicked...
+  if (click_count == 0){
+    $(this).find('p').css("opacity", 1); // shows whats behind the card
+    click_count = 1;
+     }
+     else{
+       
+     }
   });
 });
