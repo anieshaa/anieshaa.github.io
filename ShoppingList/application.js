9a4b2a3509d1017a58f0577985d694f5af4a502e
anieshaa <aallen1@kippnyccp.org>
2015-04-15 09:17:34 -0400

    Update application.js

--- ShoppingList/application.js
+++ ShoppingList/application.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function(){
+  $('button').on('click', function(){ //when you click on the button
+    var entry = $('input').val(); //finds the value of what is entered into the box
+    $('ul').append("<li>"+entry+"</li>");//adds entry to the list
+  })
+});
